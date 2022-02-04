097c34e856f9f90f73645f3790bacc368440eb1b
Valentin ALLART <allart.valentin78@gmail.com>
2022-02-04 11:07:42 +0100

    Traduction + twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,11 +7,12 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(ManagerRegistry $d): Response
+    public function index(ManagerRegistry $d, TranslatorInterface $t): Response
     {   
         $em = $d->getManager();
 
